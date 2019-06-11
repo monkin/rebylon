a67f6b5afdb892e918bac1f1ddc045cd8c3ef682
Andrey Monkin <amonkin@evolutiongaming.com>
2019-06-11 16:14:34 +0300

    Tests for param changes/stream

--- src/param.test.ts
+++ src/param.test.ts
@@ -1,4 +1,4 @@
-import { map, isMapping, unwrap, isValue } from "./param";
+import { map, isMapping, unwrap, isValue, changes, stream } from "./param";
 
 describe("Param", () => {
     it("should destinguish values and mappings", () => {
@@ -31,5 +31,74 @@ describe("Param", () => {
         const m2 = map(2, () => 3, () => "_test")((a, b, c) => (a * b) + c);
         expect(isMapping(m2)).toBe(true);
         expect(unwrap(m2)).toBe("6_test");
+    });
+
+    it("should stream changes", () => {
+        let a = 1,
+            b = "x";
+        const r: { a: number, b: string }[] = [],
+            updater = changes(() => a, () => b)((a, b) => {
+                r.push({ a, b });
+            });
+        
+        expect(r).toEqual([{ a: 1, b: "x" }]);
+        
+        updater();
+        updater();
+        expect(r).toEqual([{ a: 1, b: "x" }]);
+
+        b = "y";
+        updater();
+        updater();
+        expect(r).toEqual([
+            { a: 1, b: "x" },
+            { a: 1, b: "y" },
+        ]);
+
+        a = 2;
+
+        updater();
+        expect(r).toEqual([
+            { a: 1, b: "x" },
+            { a: 1, b: "y" },
+            { a: 2, b: "y" },
+        ]);
+
+        updater();
+        expect(r).toEqual([
+            { a: 1, b: "x" },
+            { a: 1, b: "y" },
+            { a: 2, b: "y" },
+        ]);
+    });
+
+
+    it("should stream changes and dispose previous state", () => {
+        let a = 1,
+            b = "x";
+        const r: string[] = [],
+            component = stream(() => a, () => b)((a, b) => {
+                r.push(`${a}_${b}`);
+                return () => r.push(`/${a}_${b}`);
+            });
+        
+        expect(r).toEqual(["1_x"]);
+
+        component.update();
+        expect(r).toEqual(["1_x"]);
+
+        a = 2;
+        component.update();
+        expect(r).toEqual(["1_x", "/1_x", "2_x"]);
+
+        component.update();
+        expect(r).toEqual(["1_x", "/1_x", "2_x"]);
+
+        b = "y";
+        component.update();
+        expect(r).toEqual(["1_x", "/1_x", "2_x", "/2_x", "2_y"]);
+
+        component.dispose();
+        expect(r).toEqual(["1_x", "/1_x", "2_x", "/2_x", "2_y", "/2_y"]);
     });
 });
